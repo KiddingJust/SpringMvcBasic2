1f49385c3d8e919afbd4dbc492f8c75749b3e19e
Kidding Just <rlarkdlr@gmail.com>
2021-10-27 19:42:49 +0900

    first commit

---
+++ src/main/java/com/example/springmvcbasic2/SpringMvcBasic2Application.java
@@ -0,0 +1,13 @@
+package com.example.springmvcbasic2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcBasic2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcBasic2Application.class, args);
+    }
+
+}
